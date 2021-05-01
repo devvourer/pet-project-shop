8e05cf8bf9a923f61d8b1e108cadd9cbbf961859
devvourer <beksultan.toktobekov@mail.ru>
2021-05-01 17:30:34 +0600

    created forms file for cart and urls file

--- cart/cart.py
+++ cart/cart.py
@@ -45,7 +45,7 @@ class Cart:
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(item['price'] * item['quantity'] for item self.cart.values())
+        return sum(item['price'] * item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
