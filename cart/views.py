3d6724138d18a5a889ef11668af79dfa3aa097ac
devvourer <beksultan.toktobekov@mail.ru>
2021-06-23 20:55:30 +0600

    fix bugs with cart detail

--- cart/views.py
+++ cart/views.py
@@ -24,9 +24,13 @@ def cart_remove(request, product_id):
 
 
 def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
-                                                                   'update': True})
+    try:
+        cart = Cart(request)
+        for item in cart:
+            item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                           'update': True})
+    except UnboundLocalError:
+        error = 'Your cart is empty'
+        return render(request, 'cart/detail.html', {'error': error})
     return render(request, 'cart/detail.html', {'cart': cart})
 
