e46414fec017c6f7cc05a04a94d0bab78161ee14
devvourer <beksultan.toktobekov@mail.ru>
2021-09-09 23:56:04 +0600

    pdf file for order

--- orders/admin.py
+++ orders/admin.py
@@ -7,10 +7,6 @@ from django.utils.safestring import mark_safe
 import csv
 import datetime
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_fields = ['product']
-
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -41,11 +37,23 @@ def order_detail(obj):
     return mark_safe('<a href="{}">View</a>'.format(reverse('orders:admin_order_detail', args=[obj.id])))
 
 
+def order_pdf(obj):
+    return mark_safe('<a href="{}">PDF</a>'.format(reverse('orders:admin_order_pdf', args=[obj.id])))
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated', order_detail]
+                    'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
+
+    order_pdf.short_description = 'Invoice'
 
